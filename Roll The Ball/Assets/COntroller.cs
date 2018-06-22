7133ece895003b5f561cf0295c00cbd1529ce8fc
ankitpriyarup <ankitpriyarup@gmail.com>
2018-06-22 13:09:57 +0530

    init

---
+++ Roll The Ball/Assets/COntroller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class COntroller : MonoBehaviour
+{
+	public float speed;
+
+	void Update ()
+	{
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+		Vector3 movement = new Vector3(horizontal, 0, vertical);
+		rigidbody.AddForce(movement * speed);
+	}
+}
---
+++ Roll The Ball/Assets/GameOver.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.name == "Player")
+		{
+			//GameOver
+			SceneManager.LoadScene("Main");
+		}
+	}
+}
